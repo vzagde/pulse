abca4d606a7ea914cc1d91b21bb773ebfb152971
dawoodahmed <adawood200999@gmail.com>
2016-11-13 20:24:20 +0530

    first commit

---
+++ application/views/admin/includes/sidebar.php
@@ -0,0 +1,79 @@
+
+    
+
+    <div class="container-fluid no-pad" style="padding-top:50px">
+
+
+
+      <div class="row" style="height: 2450px;">  
+        <div  class="col-md-2 left-panel">
+          
+
+          <div class="nav-container">
+          <br>
+          	<a class="nav-anchor" href="<?php echo base_url()?>admin">
+          		<h4>DASHBOARD</h4>
+          	</a>
+            <a class="nav-anchor" href="<?php echo base_url()?>admin/entities">
+              <h4>ENTITIES</h4>
+            </a>
+
+            <a class="nav-anchor" href="<?php echo base_url()?>admin/events">
+              <h4>EVENTS</h4>
+            </a>
+
+
+            <a class="nav-anchor" href="<?php echo base_url()?>admin/clubs">
+              <h4>CLUBS</h4>
+            </a>
+
+
+            <a class="nav-anchor" href="<?php echo base_url()?>admin/location">
+              <h4>LOCATION</h4>
+            </a>
+
+
+            <a class="nav-anchor" href="<?php echo base_url()?>admin/offers">
+              <h4>OFFERS</h4>
+            </a>
+
+
+            <a class="nav-anchor" href="<?php echo base_url()?>admin/eventtype_copy">
+              <h4>EVENT TYPES</h4>
+            </a>
+
+
+            <a class="nav-anchor" href="<?php echo base_url()?>admin/reviews">
+              <h4>REVIEWS</h4>
+            </a>
+
+
+            <a class="nav-anchor" href="<?php echo base_url()?>admin/points">
+              <h4>POINTS</h4>
+            </a>
+
+
+            <a class="nav-anchor" href="<?php echo base_url()?>admin/transaction">
+              <h4>TRANSACTION</h4>
+            </a>
+
+
+            <a class="nav-anchor" href="<?php echo base_url()?>admin/notification_panel">
+              <h4>NOTIFICATION</h4>
+            </a>
+
+
+            <a class="nav-anchor" href="<?php echo base_url()?>admin/users">
+              <h4>USERS</h4>
+            </a>
+
+
+
+
+          
+          	
+          </div>
+
+          
+
+        </div>
---
+++ application/views/admin/login.php
@@ -0,0 +1,87 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Authencation</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.6 -->
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/AdminLTE.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="<?php echo base_url();?>assets/css/blue.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <!-- <a href="../../index2.html"><b>Admin</b>LTE</a> -->
+    <h3>Admin Panel</h3>
+    <!-- <img width="50%" src="<?php echo base_url();?>assets/img/logo.png"> -->
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">
+      Sign in to start your session
+      <?php if(isset($message)){ echo $message;} ?>
+    </p>
+
+    <form action="<?php echo base_url()?>auth/login" method="post">
+
+      <div class="form-group has-feedback">
+        <input name="identity" type="email" class="form-control" placeholder="Email">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input name="password" type="password" class="form-control" placeholder="Password">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-12">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+        </div>
+      </div>
+      <!-- <br> -->
+      <!-- <a style="padding-top: 10px" href="<?= base_url()?>auth/forgot_password">I forgot my password</a><br> -->
+    </form>
+
+    <!-- /.social-auth-links -->
+
+
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery 2.2.3 -->
+<!-- <script src="../../plugins/jQuery/jquery-2.2.3.min.js"></script> -->
+<script src="http://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+<!-- Bootstrap 3.3.6 -->
+<!-- <script src="../../bootstrap/js/bootstrap.min.js"></script> -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<!-- iCheck -->
+<script src="<?php echo base_url();?>/assets/js/icheck.min.js"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' // optional
+    });
+  });
+</script>
+</body>
+</html>
